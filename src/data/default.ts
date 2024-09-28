9c8bad2f2350e010929b27c1107fb7035a120bd2
Chris White <chris@chriswhite.rocks>
2024-09-28 13:00:50 -0700

    clean up typescript linting caused failed deployment

--- src/data/default.ts
+++ src/data/default.ts
@@ -1,10 +1,11 @@
-export const defaultQuizData: Data = {
+export const defaultQuizData: QuizData = {
   quiz_name: "Linux Commands Quiz",
   quiz_id: 10001,
   category_id: 1,
   category_name: "linux",
   questions: [
     {
+      questionId: 1,
       question: "What does the 'ls' command do?",
       choices: {
         A: "Moves a file",
@@ -13,6 +14,7 @@ export const defaultQuizData: Data = {
         D: "Lists directory contents",
       },
       correctAnswer: "D",
+      chosenAnswer: "",
     },
   ],
 };
